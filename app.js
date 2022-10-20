62239e80acd33ea29a7ee36cbec5c1da2cc2ddc2
braverokmc79 <braverokmc79@gmail.com>
2022-10-20 14:05:29 +0900

    6.라우팅 분리

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+//모듈
 const express = require("express");
 const app = express();
+
+const PORT = 3000;
+
+//라우터
+const home = require("./routes/home");
+
 
 //앱 셋팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("home/index");
-});
+app.use("/", home); //use ->미들웨어를 등록해 주는 메소드.
 
-app.get("/login", (req, res) => {
-    res.render("home/login");
-})
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log("서버 가동");
 });
