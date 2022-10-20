1d0234bf9932beb2b60ba7255dfcc8ffec417003
braverokmc79 <braverokmc79@gmail.com>
2022-10-20 18:48:35 +0900

    9.package.json | package-lock.json | node_modules | npm start

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+
 
 //라우터
 const home = require("./routes/home");
@@ -15,6 +15,4 @@ app.set("view engine", "ejs");
 app.use("/", home); //use ->미들웨어를 등록해 주는 메소드.
 
 
-app.listen(PORT, function () {
-    console.log("서버 가동");
-});
+module.exports = app;
