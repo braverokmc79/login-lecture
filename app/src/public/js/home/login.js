9bcd3582e63a245c380c7184850be6ca7ab3ea09
braverokmc79 <braverokmc79@gmail.com>
2022-10-20 22:42:30 +0900

    17.로그인 인증 기능 만들기 in 서버 | 유저 데이터 만들기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -5,17 +5,19 @@ const loginBtn = document.querySelector("button");
 
 loginBtn.addEventListener("click", login);
 
-function login() {
+async function login() {
     const req = {
         id: id.value,
         psword: psword.value
     }
 
-    fetch("/login", {
+    const result = fetch("/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(req)
-    })
+    }).then(res => res.json())
+        .then(res => res)
+    console.log("result : ", result);
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,9 +10,27 @@ const output = {
     },
 }
 
+
+const users = {
+    id: ["test1", "test2", " test3"],
+    psword: ["1111", "1111", "1111"]
+}
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id;
+        const psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({ success: true })
+            }
+
+        }
+
+        return res.json({ success: false, msg: "로그인에 실패하였습니다." })
+
     }
 }
 
