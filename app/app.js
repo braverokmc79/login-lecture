ca44e325fbe24dae4defb74e06f68a2fe4598e81
braverokmc79 <braverokmc79@gmail.com>
2022-10-22 23:31:30 +0900

    35.로그 관리 | morgan (1/4)

--- app/app.js
+++ app/app.js
@@ -2,13 +2,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
-
-
+const morgan = require('morgan')
 const app = express();
+const accessLogStream = require("./src/config/log");
+
+dotenv.config();
 
 //라우터
 const home = require("./src/routes/home");
+
+
+
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 
 //앱 셋팅
@@ -18,7 +25,14 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(morgan('dev'));
+app.use(morgan('common', { stream: accessLogStream }));
+
+
 app.use("/", home); //use ->미들웨어를 등록해 주는 메소드.
 
 
+
 module.exports = app;
+
+
